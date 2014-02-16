6fbba6c111d68fda439ef2092935872bdebf68f5
jefferson94 <ferneyvega1994@gmail.com>
2014-02-16 16:42:46 -0500

    soluccion ejemplos propuestos en clase

---
+++ src/ufps/gui/TestMatriz.java
@@ -0,0 +1,49 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ufps.gui;
+import ufps.negocio.MatrizEnteros;
+
+/**
+ *
+ * @author MADARME
+ */
+public class TestMatriz {
+    
+        public static void main(String nada[])
+        {
+            MatrizEnteros m=new MatrizEnteros(3,5);
+            System.out.println(m.toString());
+            
+            System.out.println("------------------------------");
+            MatrizEnteros m2=new MatrizEnteros(3,0);
+            System.out.println(m2.toString());
+       
+            boolean esDispersa = m2.esDispersa();
+        System.out.println(esDispersa);
+        
+         int v1[]= {0,0,3}; 
+         
+         int v2[]= {0,4,3}; 
+         
+         
+         boolean a = vectoresIguales(v1,v2);
+         System.out.println(a);
+        }
+
+    private static boolean vectoresIguales(int[] v1, int[] v2) {
+          for (int i=0; i<v1.length;i++){
+        
+            if (v1[i]!=v2[i]) {
+                return false;
+            }
+        }
+        
+        
+        return true;
+    
+    }
+    
+    
+}
---
+++ src/ufps/negocio/MatrizEnteros.java
@@ -0,0 +1,175 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ufps.negocio;
+
+/**
+ *
+ * @author MADARME
+ */
+public class MatrizEnteros {
+    
+    private int matriz[][];
+    private int length;
+
+    public MatrizEnteros() {
+    }
+
+    public MatrizEnteros(int filas , int columnas) {
+        
+       if(columnas==0)
+       {
+           //creando matrices dispersas
+           this.matriz=new int[filas][];
+           crearElementosDispersos();
+       }
+       else
+       {
+        this.matriz = new int[filas][columnas];
+        crearElementos();
+       }
+    }
+    
+    
+    private void crearElementosDispersos()
+    {
+        for(int i=0;i<this.matriz.length;i++)
+        {
+            int columnas=(int)(Math.random()*this.matriz.length+1);
+            this.matriz[i]=new int[columnas];
+            crearElementosVector(this.matriz[i]);
+        }
+    }
+    
+    
+    
+    private void crearElementos()
+    {
+        for(int i=0;i<this.matriz.length;i++)
+        {
+            crearElementosVector(this.matriz[i]);
+        }
+        
+    }
+    
+    
+    private void crearElementosVector(int vectorCols[])
+    {
+        for(int j=0;j<vectorCols.length;j++)
+        {
+            int elemento=(int)(Math.random()*vectorCols.length);
+            vectorCols[j]=elemento;
+        }
+    }
+    
+    
+    
+    //foreach o iterador
+    public String toString()
+    {
+    String msg="";
+        for(int vectorCols[]:this.matriz)
+        {
+            for(int elemento:vectorCols)
+            {
+                msg+=elemento+"\t";
+            }
+            msg+="\n";
+        }
+     return (msg);
+    }
+    
+    
+    //manera convencional
+    public String toString2()
+    {
+    String msg="";
+        for(int i=0; i<this.matriz.length;i++)
+        {
+            for(int j=0;j<this.matriz[i].length;j++)
+            {
+                msg+=this.matriz[i][j]+"\t";
+            }
+            msg+="\n";
+        }
+     return (msg);
+    }
+    
+    
+    
+    /*
+     * @return true si la matriz es dispersa o false en caso contrario
+     */
+    public boolean esDispersa()
+            {
+
+             int tamaño = matriz[0].length;
+             int i =0;
+           
+             while (matriz[i].length!=tamaño){
+            
+               return true;
+        
+             }
+             return false ;
+            }
+    
+    
+    /**
+     * Compara si dos objetos de la clase MatrizEnteros son iguales
+     * @param obj la matriz2 a comparar
+     * @return 
+     */
+    public boolean equals(Object obj)
+    {
+        MatrizEnteros m2=(MatrizEnteros)obj;
+      
+        boolean a = this.esDispersa();
+        boolean b= m2.esDispersa();
+       int m4[][]= this.matriz;
+        
+        if ( this.matriz.length==m2.matriz.length){
+        
+            if( a==b) {
+               
+                int i =0 ;
+                while ( i < this.length){
+                
+                 
+         //boolean s = vectoresIguales(m4[i],m2[i]);
+
+                if( s== true ){
+                
+                return true ;
+                
+                }
+                
+                }
+            
+               }
+            return false;
+            }
+        
+        return false;
+    }
+    
+    public boolean vectoresIguales ( int m1[] ,int m2[]){
+       
+     
+        for (int i=0; i<m1.length;i++){
+        
+            if (m1[i]!=m2[i]) {
+                return false;
+            }
+        }
+        
+        
+        return true;
+    
+    
+   }
+
+}
+    
+
